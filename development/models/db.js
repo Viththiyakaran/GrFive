c1d41197fd9142b2aa18ecbab4f9cd173a46d07e
Viththiyakaran <battikaran6@gmail.com>
2021-01-03 06:15:31 +0530

    Add mongodb claster connection

--- development/models/db.js
+++ development/models/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
-const URL = 'mongodb://localhost/Grfive';
+const URL = 'mongodb+srv://grfive_app:94@Viththi@cluster0.mstro.mongodb.net/<dbname>?retryWrites=true&w=majority';
 mongoose.connect(URL, { useNewUrlParser: true , useUnifiedTopology: true});
 const con = mongoose.connection;
 
 con.on('open', function(){
     console.log('DB connected');
 });
+
+
+
+//mongodb+srv://grfive_app:94@Viththi@cluster0.mstro.mongodb.net/<dbname>?retryWrites=true&w=majority
