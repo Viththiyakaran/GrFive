7cdac7e010220ca995b93e40221abb9e054f5f5d
Viththiyakaran <battikaran6@gmail.com>
2021-02-21 08:12:38 +0800

    Iinit react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from'./components/navbar';
+import Company from  './components/company';
+function App() {
+  return (
+   <div> 
+     <Navbar/>
+     <Company/>
+   </div>
+  );
+}
+
+export default App;
