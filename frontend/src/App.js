6d29e002675258ab85690d40c67f24f9d6c5ec14
Viththiyakaran <battikaran6@gmail.com>
2021-02-21 08:53:05 +0800

    Add additinal one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from'./components/navbar';
-import Company from  './components/support';
+import Company from './components/support';
 import Features from  './components/features';
+import Additional from  './components/additional';
 function App() {
   return (
    <div> 
      <Navbar/>
      <Company/>
      <Features/>
+     <Additional/>
    </div>
   );
 }
