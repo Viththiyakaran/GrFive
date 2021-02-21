e174111303e4a20b758b56acfbba6b223f6ed82c
Viththiyakaran <battikaran6@gmail.com>
2021-02-21 09:14:20 +0800

    Add important compoents for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,13 @@ import Navbar from'./components/navbar';
 import Company from './components/support';
 import Features from  './components/features';
 import Additional from  './components/additional';
+import Additional_2 from  './components/additional_2';
+import Choose from  './components/choose';
+import Pricing from  './components/pricing';
+import Testimonial from  './components/testimonial';
+import Faq from  './components/faq';
+import Download from  './components/download';
+import Footer from  './components/footer';
 function App() {
   return (
    <div> 
@@ -11,6 +18,13 @@ function App() {
      <Company/>
      <Features/>
      <Additional/>
+     <Additional_2/>
+     <Choose/>
+     < Pricing/>
+     <Testimonial/>
+     <Faq/>
+     <Download/>
+     <Footer/>
    </div>
   );
 }
